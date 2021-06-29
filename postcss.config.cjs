81ac2d93ea499fee9c7633741ee7d0d534d9fbbb
dodiameer <mtxshiftg@gmail.com>
2021-06-29 21:54:57 +0300

    initial commit

---
+++ postcss.config.cjs
@@ -0,0 +1,22 @@
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+
+const config = {
+	plugins: [
+		autoprefixer(),
+		// @ts-ignore - Says it's not callable but it is
+		!dev && purgecss({
+			content: ["./src/**/*.html", "./src/**/*.svelte"]
+		}),
+
+		!dev && cssnano({
+			preset: "default",
+		}),
+	],
+};
+
+module.exports = config;
